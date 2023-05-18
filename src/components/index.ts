13a0fc3fad817cf6f723aa9de7190df56cfa6beb
sojiwoo <58384366+jiwooproity@users.noreply.github.com>
2023-05-18 17:00:41 +0900

    feature: ScanRate 입력 속도 체크 기능 추가
    입력 속도를 계산하여 키보드 UI 우측 상단에 표기하도록 Log 컴포넌트 추가

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,9 +1,13 @@
 import Container from "./Container";
 
+import Log from "./log/Log";
+
 import KeyBoard from "./keyBoard/KeyBoard";
 import KeyBoardRow from "./keyBoard/KeyBoardRow";
 import KeyBoardControl from "./keyBoard/KeyBoardControl";
 
 export { Container }
+
+export { Log }
 
 export { KeyBoardControl, KeyBoard, KeyBoardRow }
